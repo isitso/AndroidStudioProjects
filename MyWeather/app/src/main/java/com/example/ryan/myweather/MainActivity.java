e78e8bd33bc149f8becbf10c7cc7b875d4d95e23
isitso <ryan033111@gmail.com>
2015-05-27 14:55:36 -0700

    MyWeather: changed WeatherInfo class
    changed WeatherInfo class because 10 days json is different than basic
    json

--- MyWeather/app/src/main/java/com/example/ryan/myweather/MainActivity.java
+++ MyWeather/app/src/main/java/com/example/ryan/myweather/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.ryan.myweather;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,8 +34,14 @@ public class MainActivity extends ActionBarActivity {
 
         pb = (ProgressBar)findViewById(R.id.progressBar);
         pb.setMax(100);
-        if (isOnline())
+        if (isOnline()) {
             pb.setProgress(50);
+            LoadWeather task = new LoadWeather();
+            task.execute();
+        }else{
+            pb.setVisibility(View.GONE);
+            Toast.makeText(this, "Not Online", Toast.LENGTH_LONG).show();
+        }
 
     }
 
@@ -53,5 +72,72 @@ public class MainActivity extends ActionBarActivity {
         ConnectivityManager cm =(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = cm.getActiveNetworkInfo();
         return (ni != null && ni.isConnected());
+    }
+
+    /*=============================================
+    helper class to load weather in background
+     ==============================================*/
+    private class LoadWeather extends AsyncTask<String, Long, Long>{
+        HttpURLConnection c;
+        WeatherInfo wi;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pb.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onProgressUpdate(Long... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Long aLong) {
+            if (aLong != 1l){
+                Log.d(Constants.TAG, "Got my result");
+            }else{
+                Toast.makeText(getApplicationContext(), "Background Task failed", Toast.LENGTH_LONG).show();
+            }
+            pb.setVisibility(View.GONE);
+        }
+
+        @Override
+        protected Long doInBackground(String... params) {
+            String api_str = "http://api.openweathermap.org/data/2.5/forecast/daily?lat=35&lon=139&cnt=10&mode=json";
+
+            try{
+                URL data_url = new URL(api_str);
+                c = (HttpURLConnection)data_url.openConnection();
+                c.connect();
+                int status = c.getResponseCode();
+
+                // check for status
+                if (status == 200) {  // OK
+                    InputStream is = c.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                    String response_str;
+                    StringBuilder sb = new StringBuilder();
+
+                    while ((response_str = br.readLine()) != null){
+                        sb = sb.append(response_str);
+                    }
+                    String weather_data = sb.toString();
+                    Log.d(Constants.TAG, weather_data);
+                    return 0l;
+                }else
+                    return 1l;
+            } catch (MalformedURLException e) {
+                Log.e(Constants.TAG, "Malformed URL");
+                e.printStackTrace();
+                return 1l;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return 1l;
+            }finally {  // close the connection
+                if (c != null)
+                    c.disconnect();
+            }
+        }
     }
 }
